9aaf1dbb20f7ceddff8d7c4f82f3a23e3e3d1712
Shira Katz <dovops@users.noreply.github.com>
2020-06-16 12:12:27 +0300

    Updates to Extension that weren't committed before

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 Simple Chrome extension to display a sidebar with key references that are relevant to the webpage.
 
-At the present moment, this is a prototype designed to work with dafhachaim.org and daf-yomi.com, by detecting the logical 'daf/amud' of gemara, and pulling in 'Ner Mitsvah/Ein Mishpat' links into a tab.
+At the present moment, this is a prototype designed to work with dafhachaim.org and daf-yomi.com, by detecting the logical 'daf/amud' of gemara, and pulling in 'Ner Mitsvah/Ein Mishpat' links into a a sidebar.
 
 Ultimately the aim is to crowdsource other websites where it's possible to recognize identity of elements on the page.  This may also be useful to replace the Sefaria Linker button which could now run automatically on whitelisted webpages, and leverage the sidebar for enhanced display.
 
 
 # Example Screenshots 
 
-![image](Expanded-Links.png)
+![image](Sefaria-Button.png)
+
+![image](Sefaria-Dockbar.png)
+
+![image](Sefaria-Modal.png)
--- contentScript.js
+++ contentScript.js
@@ -1,32 +1,55 @@
 
 function getLinks(txt){
-  console.log("https://www.sefaria.org.il/api/links/"+txt);
+  console.log("//www.sefaria.org.il/api/links/"+txt);
+  $.get("//www.sefaria.org.il/api/links/"+txt).done(function(data){
+     data=data.filter(function(x){
+         return x.type=="ein mishpat / ner mitsvah";
+      });
+  var html="<div id='sef-sidebar-ext-btn2'></div><div id='sef-sidebar-ext' ><div class='logowrap'><div id='sef-sidebar-ext-btn'></div></div><ul>";
+  html+="<li style='color:red;font-weight:bold'><a class='_sefaria-link' data-link='//www.sefaria.org.il/"+txt+"?lang=he&with=all&lang2=he'>"+txt+"</a></li>";
 
-    $.get("https://www.sefaria.org.il/api/links/"+txt).done(function(data){
-       data=data.filter(function(x){
-           return x.type=="ein mishpat / ner mitsvah";
-        });
-  
-    var html="<div id='sef-sidebar-ext-btn2'></div><div id='sef-sidebar-ext' ><div class='logowrap'><div id='sef-sidebar-ext-btn'></div></div><ul>";
-    html+="<li style='color:red;font-weight:bold'><a target='_sefaria' href='http://www.sefaria.org.il/"+txt+"?lang=he&with=all&lang2=he'>"+txt+"</a></li>";
+$.each(data,function(i,item){
+  console.log(item.ref);
+  console.log(item.sourceHeRef);
+  console.log("//www.sefaria.org.il/"+item.sourceHeRef)
+  html+="<li><a class='_sefaria-link' data-link='//www.sefaria.org.il/"+item.sourceHeRef+"?lang=he&lang2=he'>"+item.sourceHeRef+"</a></li>";
+});
+html+="</ul></div>";
+html+="<div id='sef-sidebar-ext-modal-viewer'><div class='modalbar'><a id='modal-sidebar-new-win'>New Window</a> | <a id='modal-sidebar-toggle'>Close</a></div><iframe id='sef-sidebar-ext-iframe' src='about:blank'></iframe><div class='loading-mask'>Loading Page...</div></div>";
+$("body").append(html);
+$(document).ready(function() {
+  $("#sef-sidebar-ext-iframe").on("load",function(){
+    $(".loading-mask").hide();
+    $("#sef-sidebar-ext-iframe").show();
+  });
+  $('#sef-sidebar-ext-btn2').on('click', function() {
+    $('#sef-sidebar-ext').toggleClass('visible',true);
+  });
+  $('#sef-sidebar-ext-btn').on('click', function() {
+    $('#sef-sidebar-ext').toggleClass('visible', false);
+    $('#sef-sidebar-ext-modal-viewer').toggleClass('visible',false);
+    $(".loading-mask").hide();
+  });
 
-  $.each(data,function(i,item){
-    console.log(item.ref);
-    console.log(item.sourceHeRef);
-    console.log("http://www.sefaria.org.il/"+item.sourceHeRef)
-    html+="<li><a target='_sefaria' href='http://www.sefaria.org.il/"+item.sourceHeRef+"?lang=he&with=all&lang2=he'>"+item.sourceHeRef+"</a></li>";
+  $('#modal-sidebar-toggle,.modalbar').on('click', function() {
+    $('#sef-sidebar-ext-modal-viewer').toggleClass('visible',false);
   });
-  html+="</ul></div>";
-  $("body").append(html);
-  $(document).ready(function() {
-    $('#sef-sidebar-ext-btn,#sef-sidebar-ext-btn2').on('click', function() {
-      $('#sef-sidebar-ext').toggleClass('visible');
-    });
+  $('#modal-sidebar-new-win').on('click', function() {
+    $('#sef-sidebar-ext-modal-viewer').toggleClass('visible',false);
+    window.open($("#sef-sidebar-ext-iframe").attr("src"));
   });
+  $('._sefaria-link').on('click',function(e){
+    $(".loading-mask").show();
+    $("#sef-sidebar-ext-iframe").hide();
+    $('#sef-sidebar-ext-iframe').attr('src',$(e.target).data('link'));
+    $('#sef-sidebar-ext-modal-viewer').toggleClass('visible',true);
+    $("#sef-sidebar-ext ul li").toggleClass('active',false);
+    $(e.target).parent().toggleClass('active',true);
+  });
+});
 
 });
 }
-
 
 // If we are opening up a Daf- we maximize and scroll it by default
 if(location.href.indexOf("daf-yomi.com/DafYomi_Page.aspx")>-1){
@@ -51,10 +74,3 @@ if(location.href.indexOf("dafhachaim.org/resources")>-1){
    getLinks(txt);
   
 }
-
-// If we are on the homepage, we inject a button to launch the 4 amudim
-if(location.href.indexOf("dafYomi.aspx")>-1){
-    var link=$('#oPageLinksBox > a').last() ;
-    var url=$(link).href;
-    $(link).append("<br><button class='clsButton' href='#'  id='OpenDapim' >עמוד+4</button> ");
- } 
