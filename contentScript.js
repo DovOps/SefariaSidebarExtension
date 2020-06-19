9ed447fee3b1529be4f798618c6c0aa3dedd453e
Dov Katz <dovops@noreply.users.github.com>
2020-06-19 07:52:27 +0300

    Added numbers to references. Fixed sidebar align/logo padding

--- contentScript.js
+++ contentScript.js
@@ -6,7 +6,7 @@ function getLinks(txt) {
     data = data.filter(function (x) {
       return x.type == "ein mishpat / ner mitsvah";
     });
-    var html = "<span id='sefaria-extension-output'><div id='sef-sidebar-ext-btn2'></div><div id='sef-sidebar-ext' ><div class='logowrap'><div id='sef-sidebar-ext-btn'></div></div><ul>";
+    var html = "<span id='sefaria-extension-output'><div id='sef-sidebar-ext-btn2'></div><div id='sef-sidebar-ext' ><div class='logowrap'><div id='sef-sidebar-ext-btn'></div></div><ol>";
     html += "<li style='color:red;font-weight:bold'><a class='_sefaria-link' data-link='//www.sefaria.org.il/" + txt + "?lang=he&with=all&lang2=he'>" + txt + "</a></li>";
 
     $.each(data, function (i, item) {
@@ -15,7 +15,7 @@ function getLinks(txt) {
       console.log("//www.sefaria.org.il/" + item.sourceHeRef)
       html += "<li><a class='_sefaria-link' data-link='//www.sefaria.org.il/" + item.sourceHeRef + "?lang=he&lang2=he'>" + item.sourceHeRef + "</a></li>";
     });
-    html += "</ul></div>";
+    html += "</ol></div>";
     html += "<div id='sef-sidebar-ext-modal-viewer'><div class='modalbar'><a id='modal-sidebar-new-win'>New Window</a> | <a id='modal-sidebar-toggle'>Close</a></div><iframe id='sef-sidebar-ext-iframe' src='about:blank'></iframe><div class='loading-mask'>Loading Page...</div></div>";
     html += "</span>";
     $("body").append(html);
@@ -45,7 +45,7 @@ function getLinks(txt) {
         $("#sef-sidebar-ext-iframe").hide();
         $('#sef-sidebar-ext-iframe').attr('src', $(e.target).data('link'));
         $('#sef-sidebar-ext-modal-viewer').toggleClass('visible', true);
-        $("#sef-sidebar-ext ul li").toggleClass('active', false);
+        $("#sef-sidebar-ext ol li").toggleClass('active', false);
         $(e.target).parent().toggleClass('active', true);
       });
     });
